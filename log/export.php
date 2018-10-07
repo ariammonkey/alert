941a86005f4320718b9ea31cc718962a4edb9559
ariammonkey <43088105+ariammonkey@users.noreply.github.com>
2018-10-07 16:47:10 +0800

    Create export.php

---
+++ log/export.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="usermodel.php" method="post">
+		<button type="submit" name="csvbtn">EXPORT TO CSV</button>
+	</form>
+</body>
+</html>
