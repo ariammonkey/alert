3a97488caa9d37df92404ed46b695662cdfd026f
ariammonkey <43088105+ariammonkey@users.noreply.github.com>
2018-10-07 15:47:56 +0800

    Add files via upload

--- log/server.php
+++ log/server.php
@@ -1,7 +1,28 @@
  <?php
- $myFile = "error.log"; 
-  $fh = fopen($myFile, 'w') or die("can't open file");
+/* if(isset($_POST['error'])){
+ 	$err1 = $_POST['error'];
+ }
+ if(is)*/
+ function logs(){
+ 	if(isset($_POST['error'])){
+ 		$myFile = "error.log"; 
+  $fh = fopen($myFile, 'a') or die("can't open file");
   $stringData =$_POST['error'] ;
   fwrite($fh, $stringData);
   fclose($fh);
+ 	}
+ 	
+ }
+ 
+ function getErrors(){
+ 	if(isset($_POST['error2'])){
+ 		$myFile = "error2.log"; 
+  $fh = fopen($myFile, 'a') or die("can't open file");
+  $stringData =$_POST['error2'] ;
+  fwrite($fh, $stringData);
+  fclose($fh);
+ 	}
+ }
+ logs();
+ getErrors();
   ?>
--- log/view.php
+++ log/view.php
@@ -6,22 +6,54 @@
 </head>
 <body>
 	
-		<button type="submit" onclick="errorlog()" >Save Log</button>
+	<button type="submit" onclick="errorlog()" >Save Log</button>
+	<button type="submit" onclick="errorlog2()" >Save Log2</button>
 	
 	<script type="text/javascript">
 		function errorlog(){
-			alert('clicked');
-			var d = new Date();
-			var error = d + "Monitoring : Failed [cheverlu]";
-			 $.ajax({
-      type:"POST",
-      data:{error : error}, 
-      url: "server.php",
-      success: function(data){
+			var date = get_Date();
+			var error = date + " Monitoring : Failed [cheverlu]\n";
+			$.ajax({
+				type:"POST",
+				data:{error : error}, 
+				url: "server.php",
+				success: function(data){
 
-      alert('Written in Log File');
-    }
-    });
+					alert('Written in Log File1');
+				}
+			});
+		}
+
+		function errorlog2(){
+
+			var date = get_Date();
+			var error2 = date + " Monitoring2 : Failed [cheverlu]\n";
+			$.ajax({
+				type:"POST",
+				data:{error2 : error2}, 
+				url: "server.php",
+				success: function(data){
+
+					alert('Written in Log File2');
+				}
+			});
+		}
+		function left_Pad(str1){
+			var newstr = ('0' + str1).slice(-2);
+			return newstr;
+		}
+		function get_Date(){
+			var d = new Date();
+			var year = d.getFullYear();
+			var curmonth = left_Pad(d.getMonth() + 1);
+			var curdate = left_Pad(d.getDate());
+			var hour = left_Pad(d.getHours());
+			var mins = left_Pad(d.getMinutes());
+			var sec = left_Pad(d.getSeconds());
+
+			var today = year + '-' + curmonth + '-' + curdate + ' ' + hour + ':' + mins + ':' + sec ;
+
+			return today;
 		}
 	</script>
 </body>
